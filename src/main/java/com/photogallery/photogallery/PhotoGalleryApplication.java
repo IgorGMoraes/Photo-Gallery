8cd8dacc6aadadff526286562f54cd4135a13ee3
IgorGMoraes <igorgomesmoraes@gmail.com>
2019-10-21 15:15:59 -0300

    First Commit

---
+++ src/main/java/com/photogallery/photogallery/PhotoGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.photogallery.photogallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoGalleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/photogallery/photogallery/PhotoGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photogallery.photogallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
